2eba3a5949a648c9634b76142ded68e876a92e3a
slim <dsslimshaddy@gmail.com>
2019-01-03 00:51:09 +0530

    fix

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-import WAValidator from 'wallet-address-validator';
-import web3Utils from 'web3-utils';
+import WAValidator from "wallet-address-validator";
 export const etherscan_api_key = "8FISWFNZET4P2J451BY5I5GERA5MZG34S2";
 export const darkColors = {
     primary: {
@@ -7,8 +6,8 @@ export const darkColors = {
         main: "#6b80c5",
         dark: "#3c50a3",
         contrastText: "#fff",
-    }
-}
+    },
+};
 export const MAX_DECIMAL = 6;
 export const MAX_DECIMAL_FIAT = 2;
 export const isTestnet = true;
@@ -17,14 +16,13 @@ export const transferABI = [{ constant: !1, inputs: [{ name: "_to", type: "addre
 
 export const getAtomicValue = (config: any, rel, base) => {
     return config[rel] ? config[rel].decimals : 10 ** config[base].assets[rel].decimals;
-}
+};
 export const getConfig = (config: any, rel: string, base: string) => {
-    return config[rel] ? config[rel] : Object.assign({
+    return config[rel] ? config[rel] : {
         explorer: config[base].explorer,
         api: config[base].api,
-        rpc: config[base].rpc,
-    }, config[base].assets[rel]);
-}
+        rpc: config[base].rpc, ...config[base].assets[rel]};
+};
 
 export const isValidAddress = (config: any, address, rel, base) => {
     let networkType = `prod`;
@@ -32,54 +30,48 @@ export const isValidAddress = (config: any, address, rel, base) => {
         networkType = `testnet`;
     }
     switch (base) {
-        case 'BTC':
+        case "BTC":
         case "NEO":
         case "NANO":
-            if (WAValidator.validate(address, rel, networkType)) {
-                return true;
-            }
+            if (WAValidator.validate(address, rel, networkType)) { return true; }
         case "XRP":
             return true;
-            break;
         default:
-            if (web3Utils.isAddress(address)) {
-                return true;
-            }
+        case "ETH":
+            if (WAValidator.validate(address, base, networkType)) { return true; }
             break;
     }
     return false;
-}
-
+};
 
 export const toBitcoinJS = (o) => {
-    return Object.assign({}, o, {
-        messagePrefix: '\x18Bitcoin Signed Message:\n', // TODO
+    return {...o, 
+        messagePrefix: "\x18Bitcoin Signed Message:\n", // TODO
         bip32: {
             public: o.versions.bip32.public,
-            private: o.versions.bip32.private
+            private: o.versions.bip32.private,
         },
         pubKeyHash: o.versions.public,
         scriptHash: o.versions.scripthash,
         wif: o.versions.private,
-        dustThreshold: null // TODO
-    })
-}
+        dustThreshold: null};
+};
 
 export const numberWithCommas = (x) => {
-    var parts = x.toString().split(".");
+    let parts = x.toString().split(".");
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return parts.join(".");
-}
+};
 export const smartTrim = (string, maxLength) => {
-    if (!string) return string;
-    if (maxLength < 1) return string;
-    if (string.length <= maxLength) return string;
-    if (maxLength == 1) return string.substring(0, 1) + '...';
+    if (!string) { return string; }
+    if (maxLength < 1) { return string; }
+    if (string.length <= maxLength) { return string; }
+    if (maxLength == 1) { return string.substring(0, 1) + "..."; }
 
-    var midpoint = Math.ceil(string.length / 2);
-    var toremove = string.length - maxLength;
-    var lstrip = Math.ceil(toremove / 2);
-    var rstrip = toremove - lstrip;
-    return string.substring(0, midpoint - lstrip) + '...'
+    let midpoint = Math.ceil(string.length / 2);
+    let toremove = string.length - maxLength;
+    let lstrip = Math.ceil(toremove / 2);
+    let rstrip = toremove - lstrip;
+    return string.substring(0, midpoint - lstrip) + "..."
         + string.substring(midpoint + rstrip);
-}
+};
